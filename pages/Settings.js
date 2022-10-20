d4ea9c956d4ed77c2e70220b65b56b836bc33476
sobitd <sobitdaniel@gmail.com>
2022-10-20 19:54:14 +0300

    updates

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,7 +1,25 @@
-import { Button } from '@mui/material';
+import { Button, CircularProgress, Typography } from '@mui/material';
 import SelectedField from '../src/Components/SelectField'
+import Axios from '../src/Hooks/Axios';
+
 
 function Settings() {
+  const {res,error,loading} = Axios({url: "" })
+  if(loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  if (error) {
+    return (
+      <Typography variant='h6' nt={20} color="red">
+        Hey, there seems to be something wrong!
+      </Typography>
+    )
+  }
   const handleSubmit = (e) => {
     e.preventDefault()
   }
--- src/Hooks/Axios.js
+++ src/Hooks/Axios.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
-const Axios = () => {
-  return (
-    <div>Axios</div>
-  )
+const Axios = ({url}) => {
+    const [res, setRes] = useState(null)
+    const [error, setError] = useState('')
+    const [loading,setLoading] = useState(true)
+
+    axios.defaults.baseURL = 
+
+    useEffect(() => {
+        const fetchData = () => {
+            axios 
+            .get(url)
+            .then(res => setRes(res.data))
+            .catch(err => setError(err))
+            .finally(() => setLoading(false))
+        }
+        fetchData()
+    }, [url])
+    
+      
+
+  return {res,error,loading}
 }
 
 export default Axios
