a8eb8e1a5a417bd11467a9f5efb1367423e8fefb
sobitd <sobitdaniel@gmail.com>
2022-10-20 17:50:08 +0300

    add handlesubmit to form in Settings.js component

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,9 +1,19 @@
+import { Button } from '@mui/material';
 import SelectedField from '../src/Components/SelectField'
 
 function Settings() {
+  const handleSubmit = e = {
+    e.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <SelectedField label="Category "/>
+      <Box mt={3} width="100%">
+        <Button fullWidth variant="contained" type="submit">
+          Get Started
+        </Button>
+      </Box>
     </form>
   )
 }
