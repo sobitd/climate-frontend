800c0c8bd3a831c6b5d0120c995b0aad3b449ac0
sobitd <sobitdaniel@gmail.com>
2022-10-21 04:15:33 +0300

    update Home.js component styling

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from "react";
 import Helmet from 'react-helmet'
+import { Link } from "react-router-dom";
+
 
 
 function Home() {
   <>
   <Helmet><title>Home</title></Helmet>
   <div id="home">
-    <section className="header">
+    <section className="wrapper">
       <div>
         <img src="https://i.pinimg.com/564x/db/c3/fb/dbc3fbc1ccfb74a1bd8697abaa7c07d8.jpg"/>
+      </div>
+      <h1>Climate Quiz App</h1>
+
+      <div className="auth-container">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
       </div>
     </section>
 
